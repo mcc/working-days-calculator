757dc1419d3c295dea3a56616f5fbbb39660bd74
jylee <wjddp3@naver.com>
2020-11-01 21:28:48 +0900

    Add responsive layout style

--- src/containers/DateCalculator.js
+++ src/containers/DateCalculator.js
@@ -1,22 +1,20 @@
-import React from 'react'; 
+import React from "react";
 
-import DateSelector from 'components/DateSelector'; 
-import Card from 'components/Card';
-import ProgressBar from 'components/ProgressBar'; 
+import DateSelector from "components/DateSelector";
+import Card from "components/Card";
+import ProgressBar from "components/ProgressBar";
 
 const DateCalculator = () => {
-    return (
-        <div className="main-content">
-            <DateSelector /> 
-            <Card align="left">
-                151 working days
-            </Card>
-            <Card align="left">
-                178 calendar days
-            </Card> 
-            <ProgressBar value={82} />
-        </div>
-    ); 
-} 
+  return (
+    <div className="main-content">
+      <div className="content-wrapper">
+        <DateSelector />
+        <Card align="left">151 working days</Card>
+        <Card align="left">178 calendar days</Card>
+        <ProgressBar value={82} />
+      </div>
+    </div>
+  );
+};
 
-export default DateCalculator; 
+export default DateCalculator;
