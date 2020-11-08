de6f6ff20177c3c7390d074ff9ab9aa52097658c
jylee <wjddp3@naver.com>
2020-11-08 18:21:32 +0900

    Add GA settings

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import 'styles/app.scss'; 
-import 'react-calendar/dist/Calendar.css'; 
-import DateCalculator from 'containers'; 
-
+import React, { useEffect } from "react";
+import "styles/app.scss";
+import "react-calendar/dist/Calendar.css";
+import DateCalculator from "containers";
+import ReactGA from "react-ga";
+import DobbyIcon from "assets/images/dobby.png";
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize("G-HXVX2W66FB");
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <div className="App">
-    <DateCalculator />
+      <DateCalculator />
+      {/* <div className="dobby-wrapper">
+        <img src={DobbyIcon} alt="dobby"/>
+      </div> */}
     </div>
   );
 }
