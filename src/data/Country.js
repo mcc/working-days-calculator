85bdcbafc7555357ea5e01eaf38c97b88aa6ec19
jylee <wjddp3@naver.com>
2020-12-20 01:42:20 +0900

    Update country list

--- src/data/Country.js
+++ src/data/Country.js
@@ -176,12 +176,355 @@ const countries = {
     name: "Ecuador",
     code: "EC",
   },
+  EE: {
+    name: "Eesti",
+    code: "EE",
+  },
+
+  FI: {
+    name: "Suomi",
+    code: "FI",
+  },
+  FO: {
+    name: "Føroyar",
+    code: "FO",
+  },
+  FR: {
+    name: "France",
+    code: "FR",
+  },
+  EC: {
+    name: "Ecuador",
+    code: "EC",
+  },
+  GA: {
+    name: "Gabon",
+    code: "GA",
+  },
+  GB: {
+    name: "United Kingdom",
+    code: "GB",
+  },
+  GD: {
+    name: "Grenada",
+    code: "GD",
+  },
+  GF: {
+    name: "Guyane",
+    code: "GF",
+  },
+  GG: {
+    name: "Guernsey",
+    code: "GG",
+  },
+  GI: {
+    name: "Gibraltar",
+    code: "GA",
+  },
+  GP: {
+    name: "Guadeloupe",
+    code: "GP",
+  },
+  GT: {
+    name: "Guatemala",
+    code: "GT",
+  },
+  GU: {
+    name: "Guam",
+    code: "GU",
+  },
+  GY: {
+    name: "Guyana",
+    code: "GY",
+  },
+  HN: {
+    name: "Honduras",
+    code: "HN",
+  },
+  HR: {
+    name: "Hrvatska",
+    code: "HR",
+  },
+  HT: {
+    name: "Haïti",
+    code: "HT",
+  },
+  HN: {
+    name: "Honduras",
+    code: "HN",
+  },
+  HU: {
+    name: "Magyarország",
+    code: "HU",
+  },
+  ID: {
+    name: "Indonesia",
+    code: "ID",
+  },
+  IE: {
+    name: "Ireland",
+    code: "IE",
+  },
+  IM: {
+    name: "Isle of Man",
+    code: "IM",
+  },
+  IS: {
+    name: "Ísland",
+    code: "IS",
+  },
+  IT: {
+    name: "Itlia",
+    code: "IT",
+  },
+  JE: {
+    name: "Jersey",
+    code: "JE",
+  },
+  IS: {
+    name: "Ísland",
+    code: "IS",
+  },
+  JM: {
+    name: "Jamaica",
+    code: "JM",
+  },
+  JP: {
+    name: "Japan",
+    code: "JP",
+  },
+  KE: {
+    name: "Kenya",
+    code: "KE",
+  },
   KR: {
     name: "Republic of Korea",
     code: "KR",
   },
+  LI: {
+    name: "Lichtenstein",
+    code: "LI",
+  },
+  LT: {
+    name: "Lietuva",
+    code: "LT",
+  },
+  LU: {
+    name: "Luxembourg",
+    code: "LU",
+  },
+  LV: {
+    name: "Latvija",
+    code: "LV",
+  },
+  MC: {
+    name: "Monaco",
+    code: "MC",
+  },
+  MD: {
+    name: "Republica Moldova",
+    code: "MD",
+  },
+  ME: {
+    name: "Crna Gora",
+    code: "ME",
+  },
+  MK: {
+    name: "Republic of Macedonia",
+    code: "MK",
+  },
+  MQ: {
+    name: "Martinique",
+    code: "MQ",
+  },
+  MT: {
+    name: "Malta",
+    code: "MT",
+  },
+  MW: {
+    name: "Malawi",
+    code: "MW",
+  },
+  MX: {
+    name: "México",
+    code: "MX",
+  },
+  MZ: {
+    name: "Moçambique",
+    code: "MZ",
+  },
+  NA: {
+    name: "Namibia",
+    code: "NA",
+  },
+  NI: {
+    name: "Nicaragua",
+    code: "NI",
+  },
+  NL: {
+    name: "Netherlands",
+    code: "NL",
+  },
+  NO: {
+    name: "Norge",
+    code: "NO",
+  },
+  NL: {
+    name: "Netherlands",
+    code: "NL",
+  },
+  NZ: {
+    name: "New Zealand",
+    code: "NZ",
+  },
+  PA: {
+    name: "Panamá",
+    code: "PA",
+  },
+  PE: {
+    name: "Perú",
+    code: "PE",
+  },
+  PH: {
+    name: "Philippines",
+    code: "PH",
+  },
+  PL: {
+    name: "Polska",
+    code: "PL",
+  },
+  PT: {
+    name: "Portugal",
+    code: "PT",
+  },
+  PY: {
+    name: "Paraguay",
+    code: "PY",
+  },
+  RE: {
+    name: "Réunion",
+    code: "RE",
+  },
+  RO: {
+    name: "Romania",
+    code: "RO",
+  },
+  RS: {
+    name: "Република Србија",
+    code: "RS",
+  },
+  RU: {
+    name: "Россия",
+    code: "RU",
+  },
+  RW: {
+    name: "Repubulika y'u Rwanda",
+    code: "RW",
+  },
+  ES: {
+    name: "Spain",
+    code: "ES",
+  },
+  SE: {
+    name: "Sverige",
+    code: "SE",
+  },
+  SG: {
+    name: "Singapore",
+    code: "SG",
+  },
+  ST: {
+    name: "St. Helena",
+    code: "ST",
+  },
+  SI: {
+    name: "Republika Slovenija",
+    code: "SI",
+  },
+  SJ: {
+    name: "Svalbard & Jan Mayen",
+    code: "SJ",
+  },
+  SK: {
+    name: "Slovenská republika",
+    code: "SK",
+  },
+  SM: {
+    name: "San Marino",
+    code: "SM",
+  },
+  SS: {
+    name: "South Sudan",
+    code: "SS",
+  },
+  SV: {
+    name: "El Salvador",
+    code: "SV",
+  },
+  TG: {
+    name: "République togolaise",
+    code: "TG",
+  },
+  TO: {
+    name: "Tonga",
+    code: "TO",
+  },
+  TR: {
+    name: "Türkiye",
+    code: "TR",
+  },
+  TZ: {
+    name: "Tanzania",
+    code: "TZ",
+  },
+  UA: {
+    name: "Україна",
+    code: "UA",
+  },
+  UG: {
+    name: "Uganda",
+    code: "UG",
+  },
+  US: {
+    name: "United States of America",
+    code: "US",
+  },
+  UY: {
+    name: "Uruguay",
+    code: "UY",
+  },
+  VA: {
+    name: "Stato della Città del Vaticanoa",
+    code: "VA",
+  },
+  VE: {
+    name: "Venezuela",
+    code: "VE",
+  },
+  VN: {
+    name: "Vietnam",
+    code: "VN",
+  },
+  XK: {
+    name: "Republika e Kosovës",
+    code: "XK",
+  },
+  YT: {
+    name: "Mayotte",
+    code: "YT",
+  },
+  ZA: {
+    name: "South Africa",
+    code: "ZA",
+  },
+  ZM: {
+    name: "Zambia",
+    code: "ZM",
+  },
+  ZW: {
+    name: "Zimbabwe",
+    code: "ZW",
+  },
 };
-
-//TODO: Eesti
 
 export default countries;
