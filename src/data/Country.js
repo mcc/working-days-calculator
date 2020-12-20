7a3c4cc94b84b27ebcc8e7d569a7a95257953555
jylee <wjddp3@naver.com>
2020-12-20 15:19:43 +0900

    Change country name in english, sort alphabetically

--- src/data/Country.js
+++ src/data/Country.js
@@ -1,530 +1,128 @@
 const countries = {
-  AD: {
-    name: "Andorra",
-    code: "AD",
-  },
-  AG: {
-    name: "Antigua and Barbuda",
-    code: "AG",
-  },
-  AI: {
-    name: "Anguilla",
-    code: "AI",
-  },
-  AO: {
-    name: "Angola",
-    code: "AO",
-  },
-  AR: {
-    name: "Argentina",
-    code: "AR",
-  },
-  AS: {
-    name: "American Samoa",
-    code: "AS",
-  },
-
-  AT: {
-    name: "Austria",
-    code: "AT",
-  },
-  AU: {
-    name: "Australia",
-    code: "AU",
-  },
-  AW: {
-    name: "Aruba",
-    code: "AW",
-  },
-  BA: {
-    name: "Bosnia and Herzegovina",
-    code: "BA",
-  },
-  BB: {
-    name: "Barbados",
-    code: "BB",
-  },
-  BE: {
-    name: "Belgique",
-    code: "BE",
-  },
-  BF: {
-    name: "Burkina Faso",
-    code: "BF",
-  },
-  BM: {
-    name: "Bermuda",
-    code: "BM",
-  },
-  BN: {
-    name: "Brunei",
-    code: "BN",
-  },
-  BO: {
-    name: "Bolivia",
-    code: "BO",
-  },
-  BR: {
-    name: "Brazil",
-    code: "BR",
-  },
-  BS: {
-    name: "Bahamas",
-    code: "BS",
-  },
-  BW: {
-    name: "Botswana",
-    code: "BW",
-  },
-  BY: {
-    name: "Belarus",
-    code: "BY",
-  },
-  BZ: {
-    name: "Belize",
-    code: "BZ",
-  },
-  CA: {
-    name: "Canada",
-    code: "CA",
-  },
-  CC: {
-    name: "Cocos Islands",
-    code: "CC",
-  },
-  CD: {
-    name: "Congo DR",
-    code: "CD",
-  },
-  CF: {
-    name: "Central African Republic",
-    code: "CF",
-  },
-  CG: {
-    name: "Republic of the Congo",
-    code: "CG",
-  },
-  CH: {
-    name: "Switzerland",
-    code: "CH",
-  },
-  CL: {
-    name: "Chile",
-    code: "CL",
-  },
-  CM: {
-    name: "Cameroun",
-    code: "CM",
-  },
-  CN: {
-    name: "China",
-    code: "CN",
-  },
-  CO: {
-    name: "Colombia",
-    code: "CO",
-  },
-  CR: {
-    name: "Costa Rica",
-    code: "CR",
-  },
-  CU: {
-    name: "Cuba",
-    code: "CU",
-  },
-  CO: {
-    name: "Colombia",
-    code: "CO",
-  },
-  CV: {
-    name: "Cabo Verde",
-    code: "CV",
-  },
-  CW: {
-    name: "Curaçao",
-    code: "CW",
-  },
-  CX: {
-    name: "Christmas Island",
-    code: "CX",
-  },
-  CY: {
-    name: "Cyprus",
-    code: "CY",
-  },
-  CZ: {
-    name: "Czech Republic",
-    code: "CZ",
-  },
-  DE: {
-    name: "Deutschland",
-    code: "DE",
-  },
-  DK: {
-    name: "Denmark",
-    code: "DK",
-  },
-  DM: {
-    name: "Dominica",
-    code: "DM",
-  },
-  DO: {
-    name: "Dominican Republic",
-    code: "DO",
-  },
-  EC: {
-    name: "Ecuador",
-    code: "EC",
-  },
-  EE: {
-    name: "Eesti",
-    code: "EE",
-  },
-
-  FI: {
-    name: "Suomi",
-    code: "FI",
-  },
-  FO: {
-    name: "Føroyar",
-    code: "FO",
-  },
-  FR: {
-    name: "France",
-    code: "FR",
-  },
-  EC: {
-    name: "Ecuador",
-    code: "EC",
-  },
-  GA: {
-    name: "Gabon",
-    code: "GA",
-  },
-  GB: {
-    name: "United Kingdom",
-    code: "GB",
-  },
-  GD: {
-    name: "Grenada",
-    code: "GD",
-  },
-  GF: {
-    name: "Guyane",
-    code: "GF",
-  },
-  GG: {
-    name: "Guernsey",
-    code: "GG",
-  },
-  GI: {
-    name: "Gibraltar",
-    code: "GA",
-  },
-  GP: {
-    name: "Guadeloupe",
-    code: "GP",
-  },
-  GT: {
-    name: "Guatemala",
-    code: "GT",
-  },
-  GU: {
-    name: "Guam",
-    code: "GU",
-  },
-  GY: {
-    name: "Guyana",
-    code: "GY",
-  },
-  HN: {
-    name: "Honduras",
-    code: "HN",
-  },
-  HR: {
-    name: "Hrvatska",
-    code: "HR",
-  },
-  HT: {
-    name: "Haïti",
-    code: "HT",
-  },
-  HN: {
-    name: "Honduras",
-    code: "HN",
-  },
-  HU: {
-    name: "Magyarország",
-    code: "HU",
-  },
-  ID: {
-    name: "Indonesia",
-    code: "ID",
-  },
-  IE: {
-    name: "Ireland",
-    code: "IE",
-  },
-  IM: {
-    name: "Isle of Man",
-    code: "IM",
-  },
-  IS: {
-    name: "Ísland",
-    code: "IS",
-  },
-  IT: {
-    name: "Itlia",
-    code: "IT",
-  },
-  JE: {
-    name: "Jersey",
-    code: "JE",
-  },
-  IS: {
-    name: "Ísland",
-    code: "IS",
-  },
-  JM: {
-    name: "Jamaica",
-    code: "JM",
-  },
-  JP: {
-    name: "Japan",
-    code: "JP",
-  },
-  KE: {
-    name: "Kenya",
-    code: "KE",
-  },
-  KR: {
-    name: "Republic of Korea",
-    code: "KR",
-  },
-  LI: {
-    name: "Lichtenstein",
-    code: "LI",
-  },
-  LT: {
-    name: "Lietuva",
-    code: "LT",
-  },
-  LU: {
-    name: "Luxembourg",
-    code: "LU",
-  },
-  LV: {
-    name: "Latvija",
-    code: "LV",
-  },
-  MC: {
-    name: "Monaco",
-    code: "MC",
-  },
-  MD: {
-    name: "Republica Moldova",
-    code: "MD",
-  },
-  ME: {
-    name: "Crna Gora",
-    code: "ME",
-  },
-  MK: {
-    name: "Republic of Macedonia",
-    code: "MK",
-  },
-  MQ: {
-    name: "Martinique",
-    code: "MQ",
-  },
-  MT: {
-    name: "Malta",
-    code: "MT",
-  },
-  MW: {
-    name: "Malawi",
-    code: "MW",
-  },
-  MX: {
-    name: "México",
-    code: "MX",
-  },
-  MZ: {
-    name: "Moçambique",
-    code: "MZ",
-  },
-  NA: {
-    name: "Namibia",
-    code: "NA",
-  },
-  NI: {
-    name: "Nicaragua",
-    code: "NI",
-  },
-  NL: {
-    name: "Netherlands",
-    code: "NL",
-  },
-  NO: {
-    name: "Norge",
-    code: "NO",
-  },
-  NL: {
-    name: "Netherlands",
-    code: "NL",
-  },
-  NZ: {
-    name: "New Zealand",
-    code: "NZ",
-  },
-  PA: {
-    name: "Panamá",
-    code: "PA",
-  },
-  PE: {
-    name: "Perú",
-    code: "PE",
-  },
-  PH: {
-    name: "Philippines",
-    code: "PH",
-  },
-  PL: {
-    name: "Polska",
-    code: "PL",
-  },
-  PT: {
-    name: "Portugal",
-    code: "PT",
-  },
-  PY: {
-    name: "Paraguay",
-    code: "PY",
-  },
-  RE: {
-    name: "Réunion",
-    code: "RE",
-  },
-  RO: {
-    name: "Romania",
-    code: "RO",
-  },
-  RS: {
-    name: "Република Србија",
-    code: "RS",
-  },
-  RU: {
-    name: "Россия",
-    code: "RU",
-  },
-  RW: {
-    name: "Repubulika y'u Rwanda",
-    code: "RW",
-  },
-  ES: {
-    name: "Spain",
-    code: "ES",
-  },
-  SE: {
-    name: "Sverige",
-    code: "SE",
-  },
-  SG: {
-    name: "Singapore",
-    code: "SG",
-  },
-  ST: {
-    name: "St. Helena",
-    code: "ST",
-  },
-  SI: {
-    name: "Republika Slovenija",
-    code: "SI",
-  },
-  SJ: {
-    name: "Svalbard & Jan Mayen",
-    code: "SJ",
-  },
-  SK: {
-    name: "Slovenská republika",
-    code: "SK",
-  },
-  SM: {
-    name: "San Marino",
-    code: "SM",
-  },
-  SS: {
-    name: "South Sudan",
-    code: "SS",
-  },
-  SV: {
-    name: "El Salvador",
-    code: "SV",
-  },
-  TG: {
-    name: "République togolaise",
-    code: "TG",
-  },
-  TO: {
-    name: "Tonga",
-    code: "TO",
-  },
-  TR: {
-    name: "Türkiye",
-    code: "TR",
-  },
-  TZ: {
-    name: "Tanzania",
-    code: "TZ",
-  },
-  UA: {
-    name: "Україна",
-    code: "UA",
-  },
-  UG: {
-    name: "Uganda",
-    code: "UG",
-  },
-  US: {
-    name: "United States of America",
-    code: "US",
-  },
-  UY: {
-    name: "Uruguay",
-    code: "UY",
-  },
-  VA: {
-    name: "Stato della Città del Vaticanoa",
-    code: "VA",
-  },
-  VE: {
-    name: "Venezuela",
-    code: "VE",
-  },
-  VN: {
-    name: "Vietnam",
-    code: "VN",
-  },
-  XK: {
-    name: "Republika e Kosovës",
-    code: "XK",
-  },
-  YT: {
-    name: "Mayotte",
-    code: "YT",
-  },
-  ZA: {
-    name: "South Africa",
-    code: "ZA",
-  },
-  ZM: {
-    name: "Zambia",
-    code: "ZM",
-  },
-  ZW: {
-    name: "Zimbabwe",
-    code: "ZW",
-  },
+  AS: { name: "American Samoa", code: "AS" },
+  AD: { name: "Andorra", code: "AD" },
+  AO: { name: "Angola", code: "AO" },
+  AI: { name: "Anguilla", code: "AI" },
+  AG: { name: "Antigua and Barbuda", code: "AG" },
+  AR: { name: "Argentina", code: "AR" },
+  AW: { name: "Aruba", code: "AW" },
+  AU: { name: "Australia", code: "AU" },
+  AT: { name: "Austria", code: "AT" },
+  BS: { name: "Bahamas", code: "BS" },
+  BB: { name: "Barbados", code: "BB" },
+  BY: { name: "Belarus", code: "BY" },
+  BE: { name: "Belgium", code: "BE" },
+  BZ: { name: "Belize", code: "BZ" },
+  BM: { name: "Bermuda", code: "BM" },
+  BO: { name: "Bolivia", code: "BO" },
+  BA: { name: "Bosnia and Herzegovina", code: "BA" },
+  BW: { name: "Botswana", code: "BW" },
+  BR: { name: "Brazil", code: "BR" },
+  BN: { name: "Brunei", code: "BN" },
+  BF: { name: "Burkina Faso", code: "BF" },
+  CM: { name: "Cameroon", code: "CM" },
+  CA: { name: "Canada", code: "CA" },
+  CV: { name: "Cape Verde", code: "CV" },
+  CF: { name: "Central African Republic", code: "CF" },
+  CL: { name: "Chile", code: "CL" },
+  CN: { name: "China", code: "CN" },
+  CC: { name: "Cocos Islands", code: "CC" },
+  CO: { name: "Colombia", code: "CO" },
+  CD: { name: "Congo DR", code: "CD" },
+  CR: { name: "Costa Rica", code: "CR" },
+  HR: { name: "Croatia", code: "HR" },
+  CU: { name: "Cuba", code: "CU" },
+  CW: { name: "Curaçao", code: "CW" },
+  CY: { name: "Cyprus", code: "CY" },
+  CZ: { name: "Czech Republic", code: "CZ" },
+  DK: { name: "Denmark", code: "DK" },
+  DM: { name: "Dominica", code: "DM" },
+  DO: { name: "Dominican Republic", code: "DO" },
+  EC: { name: "Ecuador", code: "EC" },
+  SV: { name: "El Salvador", code: "SV" },
+  EE: { name: "Estonia", code: "EE" },
+  FO: { name: "Faroe Islands", code: "FO" },
+  FI: { name: "Finland", code: "FI" },
+  FR: { name: "France", code: "FR" },
+  GA: { name: "Gabon", code: "GA" },
+  DE: { name: "Germany", code: "DE" },
+  GD: { name: "Grenada", code: "GD" },
+  GP: { name: "Guadeloupe", code: "GP" },
+  GU: { name: "Guam", code: "GU" },
+  GT: { name: "Guatemala", code: "GT" },
+  GG: { name: "Guernsey", code: "GG" },
+  GF: { name: "Guiana", code: "GF" },
+  GY: { name: "Guyana", code: "GY" },
+  HT: { name: "Haïti", code: "HT" },
+  HN: { name: "Honduras", code: "HN" },
+  HU: { name: "Hungary", code: "HU" },
+  IS: { name: "Iceland", code: "IS" },
+  ID: { name: "Indonesia", code: "ID" },
+  IE: { name: "Ireland", code: "IE" },
+  IM: { name: "Isle of Man", code: "IM" },
+  IT: { name: "Itlay", code: "IT" },
+  JM: { name: "Jamaica", code: "JM" },
+  JP: { name: "Japan", code: "JP" },
+  JE: { name: "Jersey", code: "JE" },
+  KE: { name: "Kenya", code: "KE" },
+  XK: { name: "Kosovo", code: "XK" },
+  RE: { name: "La Réunion", code: "RE" },
+  LV: { name: "Lavita", code: "LV" },
+  LI: { name: "Lichtenstein", code: "LI" },
+  LT: { name: "Lithuania", code: "LT" },
+  LU: { name: "Luxembourg", code: "LU" },
+  MW: { name: "Malawi", code: "MW" },
+  MT: { name: "Malta", code: "MT" },
+  MQ: { name: "Martinique", code: "MQ" },
+  YT: { name: "Mayotte", code: "YT" },
+  MX: { name: "Mexico", code: "MX" },
+  MD: { name: "Moldova", code: "MD" },
+  MC: { name: "Monaco", code: "MC" },
+  ME: { name: "Montenegro", code: "ME" },
+  MZ: { name: "Mozambique", code: "MZ" },
+  NA: { name: "Namibia", code: "NA" },
+  NL: { name: "Netherlands", code: "NL" },
+  NZ: { name: "New Zealand", code: "NZ" },
+  NI: { name: "Nicaragua", code: "NI" },
+  MK: { name: "North Macedonia", code: "MK" },
+  NO: { name: "Norway", code: "NO" },
+  PA: { name: "Panamá", code: "PA" },
+  PY: { name: "Paraguay", code: "PY" },
+  PE: { name: "Perú", code: "PE" },
+  PH: { name: "Philippines", code: "PH" },
+  PL: { name: "Poland", code: "PL" },
+  PT: { name: "Portugal", code: "PT" },
+  CG: { name: "Republic of the Congo", code: "CG" },
+  RO: { name: "Romania", code: "RO" },
+  RU: { name: "Russia", code: "RU" },
+  RW: { name: "Rwanda", code: "RW" },
+  ST: { name: "Saint Helena", code: "ST" },
+  SM: { name: "San Marino", code: "SM" },
+  RS: { name: "Serbia", code: "RS" },
+  SG: { name: "Singapore", code: "SG" },
+  SK: { name: "Slovakia", code: "SK" },
+  SI: { name: "Slovenia", code: "SI" },
+  ZA: { name: "South Africa", code: "ZA" },
+  KR: { name: "South Korea", code: "KR" },
+  SS: { name: "South Sudan", code: "SS" },
+  ES: { name: "Spain", code: "ES" },
+  SJ: { name: "Svalbard & Jan Mayen", code: "SJ" },
+  SE: { name: "Sweden", code: "SE" },
+  CH: { name: "Switzerland", code: "CH" },
+  TZ: { name: "Tanzania", code: "TZ" },
+  TG: { name: "Togo", code: "TG" },
+  TO: { name: "Tonga", code: "TO" },
+  TR: { name: "Turkey", code: "TR" },
+  UG: { name: "Uganda", code: "UG" },
+  UA: { name: "Ukraine", code: "UA" },
+  GB: { name: "United Kingdom", code: "GB" },
+  US: { name: "United States of America", code: "US" },
+  UY: { name: "Uruguay", code: "UY" },
+  VA: { name: "Vatican City", code: "VA" },
+  VE: { name: "Venezuela", code: "VE" },
+  VN: { name: "Vietnam", code: "VN" },
+  ZM: { name: "Zambia", code: "ZM" },
+  ZW: { name: "Zimbabwe", code: "ZW" },
 };
 
 export default countries;
