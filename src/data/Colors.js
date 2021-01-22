f81f429ebf2d8b45b32db7de718caaa934f48376
jylee <wjddp3@naver.com>
2021-01-22 14:43:39 +0900

    Update styles to use theme variables

---
+++ src/data/Colors.js
@@ -0,0 +1,38 @@
+const Themes = {
+  blueTheme: [
+    "--app-background : #E2ECFE",
+    "--text : #1B256D",
+    "--active : #4360FB",
+    "--hover-background : #e2ecfe85",
+    "--progress-bar-background : #4360FB", 
+    "--gradient-100 : #87aaee",
+    "--gradient-200 : #919df3",
+    "--gradient-300 : #9794f3",
+    "--primary-100 : #3f51b5", 
+    "--primary-200 : #303f9f",
+    "--primary-300 : #1B256D",
+    "--card-box-shadow : lightgrey",
+    "--calendar-marker : #8ea0f2",
+    "--copyright-color : #1B256D"
+  ],
+  greenTheme: [
+    "--app-background : #DDE5E7",
+    "--text : #253838",
+    "--active : #609F9F",
+    "--hover-background: #609f9f21",
+    "--progress-bar-background : #6ca69a", 
+    "--gradient-100 : #419A88",
+    "--gradient-200 : #348476",
+    "--gradient-300 : #296f65",
+    "--primary-100 : #609F9F",
+    "--primary-200 : #446969",
+    "--primary-300 : #253838",
+    "--card-box-shadow : lightgrey",
+    "--calendar-marker : #348476",
+    "--copyright-color : white"
+  ],
+  redYellowTheme: [],
+  darkTheme: [],
+};
+
+export default Themes;
